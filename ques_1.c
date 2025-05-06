3a578722a7dfde26a041bcf7014ce4511198eec5
Boodenator <boode1000@gmail.com>
2025-05-06 12:08:19 +0300

    call function

--- ques_1.c
+++ ques_1.c
@@ -19,12 +19,23 @@ s_list* createNode(int data) {
     return newNode;
 }
 
-int function_list(s_list *head) {
+int print_list(s_list *head) {
     s_list * current = head;
     while (current != NULL)
     {
         printf("%d", current->data);
         current = current->next;
     }
+
+}
+
+int main() {
+    s_list *head = NULL;
+    head = createNode(1);
+    head->next = createNode(2);
+    head->next->next = createNode(3);
+
+    print_list(head);
+    return (0);
 
 }
